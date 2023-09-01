089e9a5546f8ceee11b01f2e24c39ea1903db77f
Alpha Romer Coma <400829150120@r3-1.deped.gov.ph>
2023-09-01 22:06:04 +0800

    refactor(home): add template

---
+++ src/components/home/about.tsx
@@ -0,0 +1,14 @@
+const About: React.FC = () => {
+	return (
+		<section id="about" className="h-screen">
+			<h2>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem debitis
+				esse sequi aliquid et repudiandae consequuntur pariatur corrupti culpa
+				perspiciatis aspernatur in excepturi, laboriosam beatae, magnam eius
+				dolore. Voluptate, explicabo?
+			</h2>
+		</section>
+	);
+};
+
+export default About;
---
+++ src/components/home/merch.tsx
@@ -0,0 +1,15 @@
+const Merch: React.FC = () => {
+	return (
+		<section id="merch" className="h-screen">
+			<h2>Merch</h2>
+			<p>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis
+				quibusdam fugiat, ipsum voluptatem odit saepe hic, temporibus doloribus
+				pariatur nulla exercitationem. Cum, laboriosam minima molestias officia
+				magnam fuga ex perferendis.
+			</p>
+		</section>
+	);
+};
+
+export default Merch;
