78c01b074a97caee3859bfffd01595551dbe71b8
Alpha Romer Coma <400829150120@r3-1.deped.gov.ph>
2023-09-10 21:43:33 +0800

    chore: add next-auth & prisma;

---
+++ types/environment.d.ts
@@ -0,0 +1,17 @@
+namespace NodeJS {
+	// eslint-disable-next-line no-unused-vars
+	interface ProcessEnv extends NodeJS.ProcessEnv {
+		MONGO_URI: string;
+		EMAIL_SERVER_HOST: string;
+		EMAIL_SERVER_PORT: string;
+		EMAIL_USER: string;
+		EMAIL_PASSWORD: string;
+
+		GOOGLE_ID: string;
+		GOOGLE_SECRET: string;
+
+		NEXTAUTH_URL: string;
+		NEXTAUTH_SECRET: string;
+		AUTH_TRUST_HOST: boolean;
+	}
+}
