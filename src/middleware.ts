cbf991dfc141a6260536cac27e76eb247d27e2d8
Alpha Romer Coma <alpha.coma.ict@gmail.com>
2023-10-31 13:44:08 +0800

    refactor: secure dashboard by middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,3 @@
-import { NextRequest, NextResponse } from "next/server";
+export { default } from "next-auth/middleware";
 
-interface RedirectMap {
-	[key: string]: string;
-}
-const redirectMap: RedirectMap = {
-	"/": "/2023",
-};
-
-const host = process.env.HOST_URL;
-export function middleware(req: NextRequest) {
-	const { pathname } = req.nextUrl;
-	const redirect = redirectMap[pathname];
-	if (redirect) return NextResponse.redirect(host + redirect);
-	return NextResponse.next();
-}
+export const config = { matcher: ["/2023/dashboard"] };
