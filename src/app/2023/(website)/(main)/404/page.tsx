bbc99798544c581fe55a1525eef312603befe12d
Alpha Romer Coma <alpha.coma.ict@gmail.com>
2023-11-04 21:14:36 +0800

    refactor: reroute error 404 page

--- src/app/2023/(website)/(main)/404/page.tsx
+++ src/app/2023/(website)/(main)/404/page.tsx
@@ -1,15 +1,24 @@
-const Error404 = () => {
+import { NextPage } from "next";
+const Error404: NextPage = () => {
 	return (
 		<div className="h-screen w-screen flex flex-col justify-center items-center text-center">
 			<h1>Error 404: Not Found</h1>
 			<p>🚧 The website is under development 🚧</p>
 			<p>
 				For site or project inquiries, contact the{" "}
-				<a href="mailto:acm.feu.it@gmail.com" className="underline">
+				<a
+					href="mailto:acm.feu.it@gmail.com"
+					className="underline"
+					target="_blank"
+				>
 					FEU Tech ACM
 				</a>{" "}
 				or the{" "}
-				<a href="mailto:202211383@fit.edu.ph" className="underline">
+				<a
+					href="mailto:202211383@fit.edu.ph"
+					className="underline"
+					target="_blank"
+				>
 					Webmaster
 				</a>
 			</p>
