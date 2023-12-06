b19278b78338ec51d9b3bd15d36b73aecf952f95
Alpha Romer Coma <alpha.coma.ict@gmail.com>
2023-12-07 01:03:02 +0800

    chore: bump deps; remove not-found

--- src/app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { useRouter } from "next/navigation";
-const Error404: React.FC = () => {
-	const router = useRouter();
-	router.push("/2023/404");
-	return <></>;
-};
-
-export default Error404;
