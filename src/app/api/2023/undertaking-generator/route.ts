9325282891778631e48e105d7adf7a508f38cf7e
Alpha Romer Coma <alpha.coma.ict@gmail.com>
2023-12-10 06:23:46 +0800

    perf: add temp pdf template generator for image vs all looped

--- src/app/api/2023/undertaking-generator/route.ts
+++ src/app/api/2023/undertaking-generator/route.ts
@@ -17,28 +17,56 @@ const uniName = "FEU Institute of Technology";
 const zip = new AdmZip();
 
 const dataPath = "public/data";
-const source = `${dataPath}/CONFIDENTIALITY-UNDERTAKING-STUDENT.pdf`;
+const source = `${dataPath}/CONFIDENTIALITY-UNDERTAKING-template.pdf`;
 const target = `${dataPath}/ACMX-Undertaking.zip`;
-
+const signaturePath = `${dataPath}/alpha_signature.png`;
+const idPath = `${dataPath}/alpha_id.jpg`;
+const pageDims = {
+	width: 612,
+	height: 792,
+};
 const fontSize = {
 	title: 7.8,
 	reg: 10,
 };
-
 const { title, reg } = fontSize;
+const { width, height } = pageDims;
+
+const createTemplate = async (signatureBytes: Buffer, idBytes: Buffer) => {
+	const templatePdf = await PDFDocument.load(await readFile(source));
+	const idImage = templatePdf.embedJpg(idBytes);
+	const sigImage = templatePdf.embedPng(signatureBytes);
+
+	const [idImagePage] = await Promise.all([idImage, sigImage]);
+
+	const secondPage = templatePdf.getPage(1);
+	const { width: sigWidth, height: sigHeight } = idImagePage.scale(0.25);
+	secondPage.drawImage(idImagePage, {
+		x: width / 2 - sigWidth / 2,
+		y: height / 2 - sigHeight / 2,
+		width: sigWidth,
+		height: sigHeight,
+	});
+
+	return templatePdf.save();
+};
 
 const generatePDF = serverWrapper(async (req) => {
 	const body: Body = await req.json();
 	const { name, studentNumber, yearLvl, program, courseCodes } = body;
 
-	const existingPdfBytes = await readFile(source);
-
+	const signatureBytesPromise = readFile(signaturePath);
+	const idBytesPromise = readFile(idPath);
+	const [signature, id] = await Promise.all([
+		signatureBytesPromise,
+		idBytesPromise,
+	]);
+	const templatePdfBytes = await createTemplate(signature, id);
 	const date = new Date().toLocaleDateString("en-PH");
 
 	const promises = courseCodes.map(async (course) => {
-		const pdfDoc = await PDFDocument.load(existingPdfBytes);
-		const firstPage = pdfDoc.getPages()[0];
-
+		const pdfDoc = await PDFDocument.load(templatePdfBytes);
+		const firstPage = pdfDoc.getPage(0);
 		firstPage.drawText(`${uniName} - ${course}`, {
 			x: 244,
 			y: 694,
@@ -77,10 +105,8 @@ const generatePDF = serverWrapper(async (req) => {
 
 	await Promise.allSettled(promises);
 
-	const zipBuffer = zip.toBuffer();
-
 	// Note: return to client and not write to disk
-	await writeFile(target, zipBuffer);
+	await writeFile(target, zip.toBuffer());
 	console.log("PDFs generated, zipped and saved to disk");
 
 	return NextResponse.json({
