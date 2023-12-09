69baf90fd55f769fa33b9dcd0e5260fb2b96b7d9
Alpha Romer Coma <alpha.coma.ict@gmail.com>
2023-12-10 02:22:51 +0800

    refactor: add adm-zip; generator working

--- src/app/api/2023/undertaking-generator/route.ts
+++ src/app/api/2023/undertaking-generator/route.ts
@@ -1,39 +1,85 @@
 import serverWrapper from "@/components/wrapper/serverWrapper";
+import AdmZip from "adm-zip";
 import { readFile, writeFile } from "fs/promises";
 import { NextResponse } from "next/server";
+import path from "path";
 import { PDFDocument } from "pdf-lib";
 
-// eslint-disable-next-line no-unused-vars
-const generatePDF = serverWrapper(async (_, res) => {
+interface Body {
+	name: string;
+	signature: string;
+	studentNumber: string;
+	yearLvl: string;
+	program: string;
+	courseCodes: string[];
+}
+
+const uniName = "FEU Institute of Technology";
+const generatePDF = serverWrapper(async (req) => {
+	const body = await req.json();
+	const { name, studentNumber, yearLvl, program, courseCodes } = body as Body;
+
 	const existingPdfBytes = await readFile(
-		"./public/data/CONFIDENTIALITY-UNDERTAKING-tests.pdf",
+		path.join("public", "data", "CONFIDENTIALITY-UNDERTAKING-STUDENT.pdf"),
 	);
-	// public/data/CONFIDENTIALITY-UNDERTAKING-tests.pdf
-	const pdfDoc = await PDFDocument.load(existingPdfBytes);
-	const pages = pdfDoc.getPages();
-	const firstPage = pages[0];
 
-	// date format in mm/dd/yyyy in Philippine timezone today
 	const date = new Date().toLocaleDateString("en-PH");
-	firstPage.drawText(date, {
-		x: 400,
-		y: 96,
-		size: 10,
+	const zip = new AdmZip();
+
+	const promises = courseCodes.map(async (course) => {
+		const pdfDoc = await PDFDocument.load(existingPdfBytes);
+		const pages = pdfDoc.getPages();
+		const firstPage = pages[0];
+
+		firstPage.drawText(`${uniName} - ${course}`, {
+			x: 244,
+			y: 694,
+			size: 7.8,
+		});
+
+		firstPage.drawText(date, {
+			x: 400,
+			y: 96,
+			size: 10,
+		});
+
+		firstPage.drawText(name, {
+			x: 400,
+			y: 136,
+			size: 10,
+		});
+
+		firstPage.drawText(`${yearLvl} / ${program}`, {
+			x: 400,
+			y: 122,
+			size: 10,
+		});
+
+		firstPage.drawText(studentNumber, {
+			x: 400,
+			y: 110,
+			size: 10,
+		});
+		const coursePdfBytes = await pdfDoc.save();
+		zip.addFile(`${course}.pdf`, Buffer.from(coursePdfBytes));
 	});
 
-	firstPage.drawText("Alpha Romer Coma", {
-		x: 400,
-		y: 136,
-		size: 10,
-	});
+	const result = await Promise.allSettled(promises);
+	console.log(result);
 
-	const pdfBytes = await pdfDoc.save();
-	await writeFile(
-		"./public/data/CONFIDENTIALITY-UNDERTAKING-res.pdf",
-		pdfBytes,
+	const zipBuffer = zip.toBuffer();
+
+	const zipPath = path.join(
+		"public",
+		"data",
+		"CONFIDENTIALITY-UNDERTAKING-alpha.zip",
 	);
-	console.log("Hello World");
-	return NextResponse.json({ message: "Hello World" });
+
+	await writeFile(zipPath, zipBuffer);
+
+	return NextResponse.json({
+		message: "PDFs generated, zipped and saved to disk",
+	});
 });
 
-export { generatePDF as GET };
+export { generatePDF as POST };
