6e7b895b19f799f4eec273e6c88c34b306d8ab05
Alpha Romer Coma <alpha.coma.ict@gmail.com>
2023-12-12 07:15:17 +0800

    refactor: migrate to @tsparticles/react

--- src/components/2023/(website)/(main)/signin/particles.tsx
+++ src/components/2023/(website)/(main)/signin/particles.tsx
@@ -1,92 +1,103 @@
-import { useCallback } from "react";
-import Particles from "react-tsparticles";
-import type { Engine } from "tsparticles-engine";
-import { loadSlim } from "tsparticles-slim";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+import { useEffect, useState } from "react";
 
 const ParticlePolygon = () => {
-	const particlesInit = useCallback(async (engine: Engine) => {
-		await loadSlim(engine);
+	const [init, setInit] = useState(false);
+
+	// this should be run only once per application lifetime
+	useEffect(() => {
+		initParticlesEngine(async (engine) => {
+			// you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+			// this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+			// starting from v2 you can add only the features you need reducing the bundle size
+			//await loadAll(engine);
+			//await loadFull(engine);
+			await loadSlim(engine);
+			//await loadBasic(engine);
+		}).then(() => {
+			setInit(true);
+		});
 	}, []);
 
 	return (
 		<div>
-			<Particles
-				id="tsparticles"
-				className="absolute top-0 left-0 w-full h-full"
-				init={particlesInit}
-				options={{
-					fullScreen: {
-						enable: false,
-					},
-					background: {
-						color: {
-							value: "#0b001a",
+			{init && (
+				<Particles
+					id="tsparticles"
+					className="absolute top-0 left-0 w-full h-full"
+					options={{
+						fullScreen: {
+							enable: false,
 						},
-					},
-					fpsLimit: 60,
-					interactivity: {
-						events: {
-							onClick: {
+						background: {
+							color: {
+								value: "#0b001a",
+							},
+						},
+						fpsLimit: 60,
+						interactivity: {
+							events: {
+								onClick: {
+									enable: true,
+									mode: "push",
+								},
+								onHover: {
+									enable: true,
+									mode: "repulse",
+								},
+							},
+							modes: {
+								push: {
+									quantity: 4,
+								},
+								repulse: {
+									distance: 200,
+									duration: 0.4,
+								},
+							},
+						},
+						particles: {
+							color: {
+								value: "#c505f5",
+							},
+							links: {
+								color: "#833fe0",
+								distance: 150,
 								enable: true,
-								mode: "push",
+								opacity: 0.5,
+								width: 1,
 							},
-							onHover: {
+							move: {
+								direction: "none",
 								enable: true,
-								mode: "repulse",
+								outModes: {
+									default: "bounce",
+								},
+								random: false,
+								speed: 6,
+								straight: false,
 							},
-							resize: true,
-						},
-						modes: {
-							push: {
-								quantity: 4,
+							number: {
+								density: {
+									enable: true,
+								},
+								value: 80,
 							},
-							repulse: {
-								distance: 200,
-								duration: 0.4,
+							opacity: {
+								value: 0.5,
+							},
+							shape: {
+								type: "circle",
+							},
+							size: {
+								value: { min: 1, max: 5 },
 							},
 						},
-					},
-					particles: {
-						color: {
-							value: "#c505f5",
-						},
-						links: {
-							color: "#833fe0",
-							distance: 150,
-							enable: true,
-							opacity: 0.5,
-							width: 1,
-						},
-						move: {
-							direction: "none",
-							enable: true,
-							outModes: {
-								default: "bounce",
-							},
-							random: false,
-							speed: 6,
-							straight: false,
-						},
-						number: {
-							density: {
-								enable: true,
-								area: 800,
-							},
-							value: 80,
-						},
-						opacity: {
-							value: 0.5,
-						},
-						shape: {
-							type: "circle",
-						},
-						size: {
-							value: { min: 1, max: 5 },
-						},
-					},
-					detectRetina: true,
-				}}
-			/>
+						detectRetina: true,
+					}}
+				/>
+			)}
 		</div>
 	);
 };
