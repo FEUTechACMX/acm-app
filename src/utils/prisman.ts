56e415c14311df1e2236f6822c63c7a26cab58e6
Alpha Romer Coma <alpha.coma.ict@gmail.com>
2023-12-12 03:28:32 +0800

    chore: add & config @t3-oss/env-nextjs

--- src/utils/prisman.ts
+++ src/utils/prisman.ts
@@ -1,12 +1,13 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-
+import { env } from "@/server/env";
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-	globalForPrisma.prisma ||
+	globalForPrisma.prisma ??
 	new PrismaClient({
-		log: ["query"],
+		log:
+			env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
 	}).$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
