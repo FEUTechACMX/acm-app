15476b20274eab3677c05a648a9febb47dda80d5
Alpha Romer Coma <alpha.coma.ict@gmail.com>
2023-12-16 10:47:35 +0800

    refactor: add undertaking generator route & credits

--- src/components/utils/undertaking/form.tsx
+++ src/components/utils/undertaking/form.tsx
@@ -2,6 +2,7 @@
 import { UndertakingBody } from "@/app/api/utils/undertaking-generator/route";
 import { UndertakingData } from "@/app/utils/undertaking/page";
 import ACMImage from "@/components/2023/(website)/(main)/_gen/image/acm";
+import regexIdNumber from "@/utils/regex/schoolId";
 import { Button, Checkbox } from "@nextui-org/react";
 import { useEffect } from "react";
 import { Controller, SubmitHandler, useForm } from "react-hook-form";
@@ -21,39 +22,40 @@ const UndertakingForm: React.FC<Props> = ({ props: { data } }) => {
 		setValue,
 	} = useForm<UndertakingBody>();
 	const onSubmit: SubmitHandler<UndertakingBody> = async (data) => {
-		// console.log(data);
+		const {
+			fullName,
+			studentNumber,
+			year,
+			program,
+			courses,
+			idImg,
+			signatureImg,
+		} = data;
 		const formData = new FormData();
-		formData.append("fullName", data.fullName);
-		formData.append("studentNumber", data.studentNumber);
-		formData.append("year", data.year);
-		formData.append("program", data.program);
-		// remove all null values
-		data.courses = data.courses.filter((course) => course);
-		formData.append("courses", JSON.stringify(data.courses));
-		if (data.idImg) {
-			console.log(data.idImg);
-			if (data.idImg instanceof FileList) {
-				// Append each file from the array to the FormData
-				for (const file of data.idImg) {
-					formData.append("idImg", file);
-				}
-			} else {
-				// Append the single file to the FormData
-				formData.append("idImg", data.idImg);
+		formData.append("fullName", fullName);
+		formData.append("studentNumber", studentNumber);
+		formData.append("year", year);
+		formData.append("program", program);
+		formData.append(
+			"courses",
+			JSON.stringify(courses.filter((course) => course)),
+		);
+		const appendFile = (field: string, file: File | FileList) => {
+			if (!file) throw new Error(`No file for field ${field}`);
+			if (!(file instanceof File) && !(file instanceof FileList)) {
+				throw new Error(`Invalid file type for field ${field}`);
 			}
-		}
-		if (data.signatureImg) {
-			if (data.signatureImg instanceof FileList) {
-				// Append each file from the array to the FormData
-				for (const file of data.signatureImg) {
-					formData.append("signatureImg", file);
-				}
-			} else {
-				// Append the single file to the FormData
-				formData.append("signatureImg", data.signatureImg);
-			}
-		}
 
+			if (file instanceof FileList) {
+				for (const f of file) {
+					formData.append(field, f);
+				}
+			} else {
+				formData.append(field, file);
+			}
+		};
+		appendFile("idImg", idImg);
+		appendFile("signatureImg", signatureImg);
 		// eslint-disable-next-line no-unused-vars
 		const res = await fetch(
 			"http://localhost:3000/api/utils/undertaking-generator",
@@ -62,12 +64,21 @@ const UndertakingForm: React.FC<Props> = ({ props: { data } }) => {
 				body: formData,
 			},
 		);
-		alert("Here");
-		// await axios.post("/api/utils/undertaking-generator", formData, {
-		// 	headers: {
-		// 		"Content-Type": "multipart/form-data",
-		// 	},
-		// });
+		if (res.ok) {
+			const blob = await res.blob();
+			const url = window.URL.createObjectURL(blob);
+
+			const link = document.createElement("a");
+			link.href = url;
+			link.download = "archive.zip";
+			link.style.display = "none";
+			document.body.appendChild(link);
+			link.click();
+			document.body.removeChild(link);
+			window.URL.revokeObjectURL(url);
+		} else {
+			// handle error
+		}
 	};
 	useEffect(() => {
 		setValue("courses", []);
@@ -83,6 +94,100 @@ const UndertakingForm: React.FC<Props> = ({ props: { data } }) => {
 			</div>
 			<h1 className="text-center">Undertaking Generator</h1>
 			<hr className="w-full" />
+			<div className="w-full">
+				<label htmlFor="fullName">Full Name:</label>
+				<input
+					type="text"
+					{...register("fullName", {
+						required: true,
+						minLength: 5,
+						maxLength: 50,
+					})}
+					placeholder="Victor Magtanggol"
+					className="px-1 py-2 sm:px-4 sm:py-2 w-full border-2 border-accents rounded-md"
+				/>
+			</div>
+			<div className="w-full">
+				<label htmlFor="studentNumber">Student Number:</label>
+				<input
+					type="text"
+					{...register("studentNumber", {
+						required: true,
+						minLength: 9,
+						maxLength: 9,
+						pattern: regexIdNumber,
+					})}
+					placeholder="20yyxxxxx"
+					className="px-1 py-2 sm:px-4 sm:py-2 w-full border-2 border-accents rounded-md"
+				/>
+			</div>
+			<div className="w-full">
+				<label htmlFor="Year/Program">Year/Program:</label>
+				<div className="flex w-full gap-2">
+					<select
+						{...register("year", {
+							required: true,
+						})}
+						className=" p-1 border-2 border-accents rounded-md flex-1"
+					>
+						<option value="1st">1st</option>
+						<option value="2nd">2nd</option>
+						<option value="3rd">3rd</option>
+						<option value="4th">4th</option>
+					</select>
+					<select
+						{...register("program", {
+							required: true,
+						})}
+						className=" p-1 border-2 border-accents rounded-md flex-1"
+					>
+						<option value="BSCSSE">BSCSSE</option>
+						<option value="BSCSDS">BSCSDS</option>
+					</select>
+				</div>
+			</div>
+
+			<div className="w-full">
+				<label htmlFor="studentNumber">Signature Upload:</label>
+				<Controller
+					name="signatureImg"
+					control={control}
+					render={({ field: { ref, name, onBlur, onChange } }) => (
+						<input
+							type="file"
+							ref={ref}
+							required
+							className="w-full rounded-md"
+							name={name}
+							onBlur={onBlur}
+							onChange={(e) => {
+								onChange(e.target.files?.[0]);
+							}}
+						/>
+					)}
+				/>
+			</div>
+
+			<div className="w-full">
+				<label htmlFor="studentNumber">ID Upload:</label>
+				<Controller
+					name="idImg"
+					control={control}
+					render={({ field: { ref, name, onBlur, onChange } }) => (
+						<input
+							type="file"
+							ref={ref}
+							required
+							className="w-full rounded-md"
+							name={name}
+							onBlur={onBlur}
+							onChange={(e) => {
+								onChange(e.target.files?.[0]);
+							}}
+						/>
+					)}
+				/>
+			</div>
 			<div className="max-h-[500px] overflow-y-auto border-b-2 border-accents w-full">
 				{data.map((course) => {
 					return (
@@ -113,116 +218,10 @@ const UndertakingForm: React.FC<Props> = ({ props: { data } }) => {
 					);
 				})}
 			</div>
-			<div className="flex justify-between w-full">
-				<Button radius="sm" variant="ghost" color="primary">
-					Back
+			<div className="w-full">
+				<Button type="submit" color="primary" variant="ghost">
+					Generate!
 				</Button>
-				<Button radius="sm" variant="ghost" color="primary">
-					Next
-				</Button>
-			</div>
-
-			{/* <div className="flex max-w-2xl"> */}
-			<div className="w-full flex gap-1 flex-col items-center justify-center">
-				<div className="w-32 h-12 relative m-auto">
-					<ACMImage />
-				</div>
-				<h1>Undertaking Generator</h1>
-				<hr className="w-full" />
-				<div className="w-full">
-					<label htmlFor="fullName">Full Name:</label>
-					<input
-						type="text"
-						{...register("fullName")}
-						className="px-1 py-2 valid:bg-accents sm:px-4 sm:py-2 w-full border-2 border-accents rounded-md"
-					/>
-				</div>
-				<div className="w-full">
-					<label htmlFor="studentNumber">Student Number:</label>
-					<input
-						type="text"
-						{...register("studentNumber")}
-						className="px-1 py-2 valid:bg-accents sm:px-4 sm:py-2 w-full border-2 border-accents rounded-md"
-					/>
-				</div>
-				<div className="w-full">
-					<label htmlFor="Year/Program">Year/Program:</label>
-					<div className="flex w-full gap-2">
-						<select
-							{...register("year")}
-							className=" p-1 border-2 border-accents rounded-md flex-1"
-						>
-							<option value="1st">1st</option>
-							<option value="2nd">2nd</option>
-							<option value="3rd">3rd</option>
-							<option value="4th">4th</option>
-						</select>
-						<select
-							{...register("program")}
-							className=" p-1 border-2 border-accents rounded-md flex-1"
-						>
-							<option value="BSCSSE">BSCSSE</option>
-							<option value="BSCSDS">BSCSDS</option>
-						</select>
-					</div>
-				</div>
-
-				<Controller
-					name="signatureImg"
-					control={control}
-					render={({ field: { ref, name, onBlur, onChange } }) => (
-						<input
-							type="file"
-							ref={ref}
-							name={name}
-							onBlur={onBlur}
-							onChange={(e) => {
-								onChange(e.target.files?.[0]);
-							}}
-						/>
-					)}
-				/>
-				{/* <input {...register("idImg")} type="file" id="idImg" /> */}
-				{/* <div className="w-full">
-					<label htmlFor="signatureImg">Signature Image:</label>
-					<input
-						type="file"
-						accept="image, .png, .jpg, .jpeg"
-						{...register("signatureImg")}
-						className="block border-2 border-accents w-full rounded-md"
-					/>
-				</div> */}
-
-				<Controller
-					name="idImg"
-					control={control}
-					render={({ field: { ref, name, onBlur, onChange } }) => (
-						<input
-							type="file"
-							ref={ref}
-							name={name}
-							onBlur={onBlur}
-							onChange={(e) => {
-								onChange(e.target.files?.[0]);
-							}}
-						/>
-					)}
-				/>
-				{/* 
-				<div className="w-full">
-					<label htmlFor="idImg">ID Image:</label>
-					<input
-						type="file"
-						accept="image, .png, .jpg, .jpeg"
-						{...register("idImg")}
-						className="block  border-2 border-accents w-full rounded-md"
-					/>
-				</div> */}
-
-				<div>
-					{/* <Button type="submit" /> */}
-					<button type="submit">Submit</button>
-				</div>
 			</div>
 		</form>
 	);
