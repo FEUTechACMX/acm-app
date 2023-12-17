4078a316a7ae426898255af5fec0bc767360e067
Alpha Romer Coma <alpha.coma.ict@gmail.com>
2023-12-17 13:57:19 +0800

    refactor: generator now uses JSON vs DB; rename page

---
+++ src/app/utils/undertaking-generator/page.tsx
@@ -0,0 +1,18 @@
+import UndertakingForm from "@/components/utils/undertaking-generator/form";
+import { NextPage } from "next";
+
+// export type UndertakingData = Awaited<ReturnType<typeof getCourses>>;
+
+const UndertakingPage: NextPage = async () => {
+	return (
+		<div className="min-h-screen w-screen">
+			<div className="flex justify-center items-center h-full relative max-w-3xl mx-auto m-16">
+				<div className="sm:p-6 rounded-lg flex p-1 w-full sm:w-9/12 gap-4 flex-col justify-center items-center sm:shadow-md sm:shadow-accents">
+					<UndertakingForm />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default UndertakingPage;
