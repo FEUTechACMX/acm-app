b699e699472b47fd642c60390cfc65acfc7551af
Alpha Romer Coma <alpha.coma.ict@gmail.com>
2023-12-23 03:24:15 +0800

    refactor: temp styles

--- src/components/2023/(website)/(main)/layout/nav/index.tsx
+++ src/components/2023/(website)/(main)/layout/nav/index.tsx
@@ -3,9 +3,18 @@ import { useAppDispatch, useAppSelector } from "@/utils/redux/hooks";
 import { toggleNav } from "@/utils/redux/slices/(website)/nav";
 import { motion } from "framer-motion";
 import Image from "next/legacy/image";
-import Link from "next/link";
+// import Link from "next/link";
 import { LinkProps } from "types/link";
+import {
+	Navbar,
+	NavbarBrand,
+	NavbarContent,
+	NavbarItem,
+	Button,
+	Link
+} from "@nextui-org/react";
 import Sound from "../side/music";
+import ACMImage from "../../_gen/image/acm";
 const links: LinkProps[] = [
 	{
 		name: "Home",
@@ -45,107 +54,166 @@ const Nav: React.FC = () => {
 	const dispatch = useAppDispatch();
 
 	return (
-		<motion.nav
-			id="home-nav"
-			animate={{
-				opacity: [0, 1],
-				transition: {
-					duration: 1,
-					delay: 2,
-				},
+		<Navbar
+			classNames={{
+				item: [
+					"flex",
+					"relative",
+					"h-full",
+					"items-center",
+					"data-[active=true]:after:content-['']",
+					"data-[active=true]:after:absolute",
+					"data-[active=true]:after:bottom-0",
+					"data-[active=true]:after:left-0",
+					"data-[active=true]:after:right-0",
+					"data-[active=true]:after:h-[2px]",
+					"data-[active=true]:after:rounded-[2px]",
+					"data-[active=true]:after:bg-primary",
+				],
 			}}
-			className="w-screen fixed p-2 border-t-4 border-panelBg border-solid z-20 flex justify-between font-bold backdrop-blur"
 		>
-			<Link href="/" className="flex justify-center items-center">
-				<div className="relative h-8 w-16">
-					<Image
-						src="/2023/media/img/logo/FIT_ACM.png"
-						alt="FEU Tech ACM Logo"
-						layout={"fill"}
-						objectFit={"contain"}
-						className="m-auto"
-					></Image>
-				</div>
-				<p className="font-bold text-xl hover:text-accents transition-colors">
-					FEU&nbsp;Tech&nbsp;ACM
-				</p>
-			</Link>
-			<div
-				className={`${
-					isOpen
-						? "fixed w-screen h-screen justify-center flex-col flex items-center bg-bg top-0 left-0 z-10"
-						: "hidden"
-				} md:flex md:static md:w-auto md:h-auto md:bg-transparent md:flex-row md:items-center md:justify-between`}
-			>
-				{links.map((link) => {
-					return (
-						<Sound
-							key={link.name}
-							events={{
-								onMouseEnter: {
-									path: "/2023/media/sfx/gun_cocking_short.mp3",
-									isLooped: false,
-								},
-								onClick: {
-									path: "/2023/media/sfx/gun_shot.mp3",
-									isLooped: false,
-								},
-							}}
-							className={`${
-								isOpen && "w-full text-center"
-							} hover:text-accents transition-colors cursor-pointer flex`}
-						>
-							<Link
-								href={`/2023${link.href}`}
-								className="font-bold text-xl w-full p-2"
-								onClick={() => dispatch(toggleNav())}
-							>
-								{link.name}
-							</Link>
-						</Sound>
-					);
-				})}
-			</div>
-			<div className="flex md:hidden z-10">
-				<button onClick={() => dispatch(toggleNav())}>
-					{isOpen ? (
-						<>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								className="h-10 w-10 text-accents hover:text-accentsDark transition-colors"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth="3"
-									d="M6 18L18 6M6 6l12 12"
-								/>
-							</svg>
-						</>
-					) : (
-						<>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								className="h-10 w-10 text-accents hover:text-accentsDark transition-colors"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth="3"
-									d="M4 6h16M4 12h16M4 18h16"
-								/>
-							</svg>
-						</>
-					)}
-				</button>
-			</div>
-		</motion.nav>
+			<NavbarBrand>
+		<div className="relative h-8 w-16">
+			<Image
+				src="/2023/media/img/logo/FIT_ACM.png"
+				alt="FEU Tech ACM Logo"
+				layout={"fill"}
+				objectFit={"contain"}
+				className="m-auto"
+			></Image>
+		</div>
+				<p className="font-bold text-inherit">FEU Tech ACM-X</p>
+			</NavbarBrand>
+			<NavbarContent className="sm:flex gap-4" justify="center">
+				<NavbarItem>
+					<Link href="#">
+						Features
+					</Link>
+				</NavbarItem>
+				<NavbarItem>
+					<Link href="#" aria-current="page">
+						Customers
+					</Link>
+				</NavbarItem>
+				<NavbarItem>
+					<Link color="foreground" href="#">
+						Integrations
+					</Link>
+				</NavbarItem>
+			</NavbarContent>
+			<NavbarContent justify="end">
+				<NavbarItem className="hidden lg:flex">
+					<Link href="#">Login</Link>
+				</NavbarItem>
+				<NavbarItem>
+					<Button as={Link} color="primary" href="#" variant="flat">
+						Sign Up
+					</Button>
+				</NavbarItem>
+			</NavbarContent>
+		</Navbar>
+
+		// <motion.nav
+		// 	id="home-nav"
+		// 	animate={{
+		// 		opacity: [0, 1],
+		// 		transition: {
+		// 			duration: 1,
+		// 			delay: 2,
+		// 		},
+		// 	}}
+		// 	className="w-screen fixed p-2 border-t-4 border-panelBg border-solid z-20 flex justify-between font-bold backdrop-blur"
+		// >
+		// 	<Link href="/" className="flex justify-center items-center">
+		// 		<div className="relative h-8 w-16">
+		// 			<Image
+		// 				src="/2023/media/img/logo/FIT_ACM.png"
+		// 				alt="FEU Tech ACM Logo"
+		// 				layout={"fill"}
+		// 				objectFit={"contain"}
+		// 				className="m-auto"
+		// 			></Image>
+		// 		</div>
+		// 		<p className="font-bold text-xl hover:text-accents transition-colors">
+		// 			FEU&nbsp;Tech&nbsp;ACM
+		// 		</p>
+		// 	</Link>
+		// 	<div
+		// 		className={`${
+		// 			isOpen
+		// 				? "fixed w-screen h-screen justify-center flex-col flex items-center bg-bg top-0 left-0 z-10"
+		// 				: "hidden"
+		// 		} md:flex md:static md:w-auto md:h-auto md:bg-transparent md:flex-row md:items-center md:justify-between`}
+		// 	>
+		// 		{links.map((link) => {
+		// 			return (
+		// 				<Sound
+		// 					key={link.name}
+		// 					events={{
+		// 						onMouseEnter: {
+		// 							path: "/2023/media/sfx/gun_cocking_short.mp3",
+		// 							isLooped: false,
+		// 						},
+		// 						onClick: {
+		// 							path: "/2023/media/sfx/gun_shot.mp3",
+		// 							isLooped: false,
+		// 						},
+		// 					}}
+		// 					className={`${
+		// 						isOpen && "w-full text-center"
+		// 					} hover:text-accents transition-colors cursor-pointer flex`}
+		// 				>
+		// 					<Link
+		// 						href={`/2023${link.href}`}
+		// 						className="font-bold text-xl w-full p-2"
+		// 						onClick={() => dispatch(toggleNav())}
+		// 					>
+		// 						{link.name}
+		// 					</Link>
+		// 				</Sound>
+		// 			);
+		// 		})}
+		// 	</div>
+		// 	<div className="flex md:hidden z-10">
+		// 		<button onClick={() => dispatch(toggleNav())}>
+		// 			{isOpen ? (
+		// 				<>
+		// 					<svg
+		// 						xmlns="http://www.w3.org/2000/svg"
+		// 						className="h-10 w-10 text-accents hover:text-accentsDark transition-colors"
+		// 						fill="none"
+		// 						viewBox="0 0 24 24"
+		// 						stroke="currentColor"
+		// 					>
+		// 						<path
+		// 							strokeLinecap="round"
+		// 							strokeLinejoin="round"
+		// 							strokeWidth="3"
+		// 							d="M6 18L18 6M6 6l12 12"
+		// 						/>
+		// 					</svg>
+		// 				</>
+		// 			) : (
+		// 				<>
+		// 					<svg
+		// 						xmlns="http://www.w3.org/2000/svg"
+		// 						className="h-10 w-10 text-accents hover:text-accentsDark transition-colors"
+		// 						fill="none"
+		// 						viewBox="0 0 24 24"
+		// 						stroke="currentColor"
+		// 					>
+		// 						<path
+		// 							strokeLinecap="round"
+		// 							strokeLinejoin="round"
+		// 							strokeWidth="3"
+		// 							d="M4 6h16M4 12h16M4 18h16"
+		// 						/>
+		// 					</svg>
+		// 				</>
+		// 			)}
+		// 		</button>
+		// 	</div>
+		// </motion.nav>
 	);
 };
 
