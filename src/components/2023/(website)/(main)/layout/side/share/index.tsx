f0eeec7f3e9c5ab0221a3143c998e06fc8bbaf51
Alpha Romer Coma <alpha.coma.ict@gmail.com>
2023-12-24 00:45:44 +0800

    fix: youtube href on share

--- src/components/2023/(website)/(main)/layout/side/share/index.tsx
+++ src/components/2023/(website)/(main)/layout/side/share/index.tsx
@@ -10,7 +10,7 @@ const socials: LinkProps[] = [
 	},
 	{
 		name: "YouTube",
-		href: "https://twitter.com/intent/tweet?text=Explore%20the%20latest%20in%20tech%20trends,%20research,%20and%20innovation.%20From%20software%20engineering%20to%20data%20science,%20we've%20got%20it%20all!%20%F0%9F%92%BB%F0%9F%9A%80%0A%0AJoin%20us%20on%20the%20journey%20of%20tech%20excellence.%20Visit%20our%20website%20now%20and%20stay%20updated%20with%20the%20future%20of%20technology!%20%F0%9F%8C%9F%20%23TechInnovation%20%23ACM%20%23FEUTech",
+		href: "https://www.youtube.com/@ACMFIT",
 		icon: <FaSquareYoutube size={50} />,
 	},
 	{
