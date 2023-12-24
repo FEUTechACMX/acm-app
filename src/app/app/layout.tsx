d807047e82aa5f26e62ab4db47936447d89a6da5
Alpha Romer Coma <alpha.coma.ict@gmail.com>
2023-12-24 18:57:29 +0800

    refactor: app & website file system w/ middleware

---
+++ src/app/app/layout.tsx
@@ -0,0 +1,12 @@
+import AppLayoutNav from "@/components/(app)/layout/nav";
+import ChildrenProps from "types/ChildrenProps";
+const AppLayout: React.FC<ChildrenProps> = ({ children }) => {
+	return (
+		<>
+			<AppLayoutNav />
+			{children}
+		</>
+	);
+};
+
+export default AppLayout;
