1e15f11e2ec3d4cdf213a8aad623f30a7a26c7e3
Alpha Romer Coma <alpha.coma.ict@gmail.com>
2023-12-25 15:14:39 +0800

    refactor: add & config centerComponent wrapper

---
+++ src/app/2023/error/page.tsx
@@ -0,0 +1,11 @@
+import CenterComponent from "@/components/2023/_gen/center/HomeCenter";
+import { NextPage } from "next";
+const SigninError: NextPage = () => {
+	return (
+		<CenterComponent>
+			<h1>Error 401: Unauthorized</h1>
+		</CenterComponent>
+	);
+};
+
+export default SigninError;
