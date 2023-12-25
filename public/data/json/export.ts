ec8fe02307d870b0fc3a464c35fae7b437048cf1
Alpha Romer Coma <alpha.coma.ict@gmail.com>
2023-12-25 18:11:46 +0800

    refactor: separate seeder to each JSON file

---
+++ public/data/json/export.ts
@@ -0,0 +1,26 @@
+import committeeJSON from "./committee.json";
+import committeeRoleJSON from "./committeeRole.json";
+import courseJSON from "./course.json";
+import courseTypeJSON from "./courseType.json";
+import departmentJSON from "./department.json";
+import eventTypeJSON from "./eventType.json";
+import organizationJSON from "./organization.json";
+import seniorPositionJSON from "./seniorPosition.json";
+import termJSON from "./term.json";
+import userTypeJSON from "./userType.json";
+import yearJSON from "./year.json";
+import yearLevelJSON from "./yearLevel.json";
+export {
+	committeeJSON,
+	committeeRoleJSON,
+	courseJSON,
+	courseTypeJSON,
+	departmentJSON,
+	eventTypeJSON,
+	organizationJSON,
+	seniorPositionJSON,
+	termJSON,
+	userTypeJSON,
+	yearJSON,
+	yearLevelJSON,
+};
