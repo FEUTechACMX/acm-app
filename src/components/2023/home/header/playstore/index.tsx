c8e2f892eb53e14bec32856abcf60a53d4c8f198
Alpha Romer Coma <alpha.coma.ict@gmail.com>
2023-12-28 20:51:30 +0800

    refactor: update microsoft store media files

--- src/components/2023/home/header/playstore/index.tsx
+++ src/components/2023/home/header/playstore/index.tsx
@@ -1,7 +1,21 @@
 import Image from "next/image";
 const HomePlayStore: React.FC = () => {
 	return (
-		<section id="playstore" className=" fixed bottom-0 left-0">
+		<section
+			id="playstore"
+			className="flex flex-col justify-center align-middle items-center fixed bottom-0 left-0"
+		>
+			<a
+				href="https://apps.microsoft.com/detail/9NN45TR81J26?launch=true
+	&mode=mini"
+			>
+				<Image
+					src="https://get.microsoft.com/images/en-us%20dark.svg"
+					width={176}
+					height={96}
+					alt="Microsoft Store Badge"
+				/>
+			</a>
 			<a href="https://play.google.com/store/apps/details?id=app.vercel.acmx.twa">
 				<Image
 					src={"/media/google-play-badge.png"}
