a3261bfd062f84197c89758bca3ab9090177830a
Alpha Romer Coma <alpha.coma.ict@gmail.com>
2023-12-29 19:12:27 +0800

    refactor: userTypeId now returned in front end

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -7,4 +7,14 @@ declare module "next-auth" {
 	 * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
 	 */
 	interface User extends User_T {}
+	interface Session {
+		user: User;
+	}
+}
+
+// nextauth.d.ts
+declare module "next-auth/jwt" {
+	interface JWT {
+		user: User;
+	}
 }
