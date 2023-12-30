1badae6498e758cbd358395b0f7c42ead476591f
Alpha Romer Coma <alpha.coma.ict@gmail.com>
2023-12-30 16:17:53 +0800

    refactor: *extend & split social code
    messy code. will probs revert in future

--- src/components/2023/layout/side/share/index.tsx
+++ src/components/2023/layout/side/share/index.tsx
@@ -1,11 +1,14 @@
-import { displaySocials } from "@/utils/socials/displaySocial";
-
+import { displayShareSocial } from "@/utils/socials/shareSocial";
 const LayoutShare: React.FC = () => {
 	return (
-		<section className="p-2">
+		<section className="p-2 hidden sm:block ">
 			<div className="flex flex-col items-center justify-center">
 				<p className="font-bold">Share</p>
-				<div className="flex flex-col gap-1">{displaySocials(40)}</div>
+				<div className="flex flex-col gap-1">
+					{displayShareSocial("Facebook", 50)}
+					{displayShareSocial("Twitter", 50)}
+					{displayShareSocial("LinkedIn", 50)}
+				</div>
 			</div>
 		</section>
 	);
---
+++ src/utils/socials/shareSocial.tsx
@@ -0,0 +1,40 @@
+import { Social, getSocial } from "./partialSocial";
+
+export const socialShare = new Map<socialName, Social>([
+	[
+		"Facebook",
+		{
+			...getSocial("Facebook"),
+			href: "https://www.facebook.com/sharer/sharer.php?u=https%3A//acmx.vercel.app",
+		},
+	],
+	[
+		"LinkedIn",
+		{
+			...getSocial("LinkedIn"),
+			href: "https://www.linkedin.com/shareArticle?mini=true&url=https%3A//acmx.vercel.app",
+		},
+	],
+	[
+		"Twitter",
+		{
+			...getSocial("Twitter"),
+			href: "https://twitter.com/share?text=Hello%20World%20from%20ACM-X%20App&url=https%3A//acmx.vercel.app&hashtags=feutechacm,acmx,app",
+		},
+	],
+]);
+// Type error due to Regex if Omit is used
+type socialName = "Facebook" | "LinkedIn" | "Twitter";
+export function displayShareSocial(
+	name: socialName,
+	iconSize: number = 50,
+): JSX.Element {
+	const social = getSocial(name, socialShare);
+	const IconComponent = social.icon as React.ElementType;
+	if (!IconComponent) throw new Error(`Icon for ${name} does not exist`);
+	return (
+		<a href={social.href} target="_blank" rel="noopener noreferrer" key={name}>
+			<IconComponent size={iconSize} />
+		</a>
+	);
+}
