fa6ae09ea74f6c44e06a67f74d055bc38f580ec5
Alpha Romer Coma <alpha.coma.ict@gmail.com>
2023-12-31 02:05:45 +0800

    feat: add committees page

---
+++ public/2023/media/img/officers/index.tsx
@@ -0,0 +1,53 @@
+import ImgGroup1 from "./group/group_1.jpg";
+import ImgGroup2 from "./group/group_2.jpg";
+import ImgSirAbe from "./execs/SIR ABE.png";
+import ImgJae from "./execs/1. JAE.png";
+import ImgRei from "./execs/2. REI.png";
+import ImgGuen from "./execs/3. GUEN.png";
+import ImgPiya from "./execs/4. PIYA.png";
+import ImgJane from "./execs/5. JANE.png";
+import ImgGlenn from "./execs/6. GLENN.png";
+
+import ImgGreg from "./marketing/7. GREG.png";
+import ImgEd from "./marketing/8. ED.png";
+import ImgFrancis from "./marketing/9. FRANCIS.png";
+import ImgEmily from "./marketing/10. EMILY.png";
+import ImgGeloy from "./marketing/11. GELOY.png";
+import ImgEarl from "./marketing/12. EARL.png";
+import ImgAnne from "./marketing/13. ANNE.png";
+import ImgAlpha from "./marketing/14. ALPHA.png";
+
+import ImgRalph from "./programs/15. RALPH.png";
+import ImgJommel from "./programs/16. JOMMEL.png";
+import ImgBeau from "./programs/17. BEAU.png";
+import ImgDaf from "./programs/18. DAF.png";
+import ImgJonner from "./programs/19. JONNER.png";
+import ImgIsaiah from "./programs/20. ISAIAH.png";
+import ImgSean from "./programs/21. SEAN.png";
+
+const Execs = [ImgSirAbe, ImgJae, ImgRei, ImgGuen, ImgPiya, ImgJane, ImgGlenn];
+
+const Marketing = [
+	ImgGreg,
+	ImgEd,
+	ImgFrancis,
+	ImgEmily,
+	ImgGeloy,
+	ImgEarl,
+	ImgAnne,
+	ImgAlpha,
+];
+
+const Programs = [
+	ImgRalph,
+	ImgJommel,
+	ImgBeau,
+	ImgDaf,
+	ImgJonner,
+	ImgIsaiah,
+	ImgSean,
+];
+
+const Groups = [ImgGroup1, ImgGroup2];
+
+export { Execs, Marketing, Programs, Groups };
