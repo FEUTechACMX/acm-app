2c9b93a2028b2bf3007832538e681a36bc2dd868
Alpha Romer Coma <alpha.coma.ict@gmail.com>
2023-12-30 16:06:44 +0800

    feat: initial contact page

--- src/components/2023/_gen/center/HomeCenter.tsx
+++ src/components/2023/_gen/center/HomeCenter.tsx
@@ -3,7 +3,7 @@ import ACMImage from "../image/ACMImage";
 const CenterComponent: React.FC<ChildrenProps> = ({ children }) => {
 	return (
 		<section className="h-svh w-svw fixed top-0 left-0">
-			<div className="flex justify-center items-center h-full relative max-w-3xl m-auto">
+			<div className="flex justify-center items-center h-full relative max-w-4xl m-auto">
 				<div className="sm:p-6 rounded-lg flex p-1 text-center w-full sm:w-9/12 gap-2 flex-col justify-center items-center sm:shadow-md sm:shadow-accents">
 					<div className="h-12 w-64 relative">
 						<ACMImage />
