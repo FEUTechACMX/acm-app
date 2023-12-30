a4ca9a6d79fa8367821ce55f87b17a9b05453452
Alpha Romer Coma <alpha.coma.ict@gmail.com>
2023-12-30 16:06:44 +0800

    feat: initial contact page

--- src/app/2023/contact/page.tsx
+++ src/app/2023/contact/page.tsx
@@ -1,22 +1,38 @@
 import CenterComponent from "@/components/2023/_gen/center/HomeCenter";
+import { displaySocials } from "@/utils/socials/displaySocial";
 const Contact2023 = () => {
 	return (
 		<CenterComponent>
 			<h1>Contact Us</h1>
-			<div>
-				<div>
-					<p>Email Address</p>
+			<div className="flex flex-col gap-2">
+				<div className="text-start text-xl">
 					<p>
-						FEU Institute of Technology Association for Computing Machinery
-						Student Chapter
+						<span className="text-bold">Address: </span>
+						<a
+							href="https://maps.app.goo.gl/jMbjHqWF3W8mFZq58"
+							target="_blank"
+							className="text-panelColor"
+						>
+							P. Paredes St. Sampaloc, Manila, Philippines, 1015
+						</a>
 					</p>
-					<a
-						href="mailto:acm.feu.it@gmail.com"
-						className="underline"
-						target="_blank"
-					>
-						acm.feu.it@gmail.com
-					</a>
+					<p>
+						Email Address:{" "}
+						<a
+							href="mailto:acm.feu.it@gmail.com"
+							target="_blank"
+							className="text-panelColor"
+						>
+							acm.feu.it@gmail.com
+						</a>
+					</p>
+				</div>
+				<hr />
+				<p className="font-2xl text-panelColor font-bold">
+					Get In Touch With Us
+				</p>
+				<div className="grid grid-cols-4 place-items-center">
+					{displaySocials(50)}
 				</div>
 			</div>
 		</CenterComponent>
