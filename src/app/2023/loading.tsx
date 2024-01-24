93dc846cafbc200a55b92a810d5ebc2e29dcb92b
Alpha Romer Coma <alpha.coma.ict@gmail.com>
2024-01-24 19:56:10 +0800

    refactor: migrate signin page to nextui

--- src/app/2023/loading.tsx
+++ src/app/2023/loading.tsx
@@ -1,7 +1,7 @@
 import { Spinner } from "@nextui-org/react";
 const Loading: React.FC = () => {
 	return (
-		<section className="h-svh w-svw flex justify-center items-center fixed z-50">
+		<section className="h-svh w-svw flex top-0 left-0 justify-center items-center fixed z-50 backdrop-blur">
 			<Spinner />
 		</section>
 	);
