2d487540f095972fa07a31ebfe3bc32c68bcb79f
Alpha Romer Coma <alpha.coma.ict@gmail.com>
2024-01-24 19:56:10 +0800

    refactor: migrate signin page to nextui

--- src/app/2023/page.tsx
+++ src/app/2023/page.tsx
@@ -1,13 +1,9 @@
-// import HomeHeader from "@/components/2023/home/header";
 import fromAppReferer from "@/utils/validators/fromAppReferer";
 import type { NextPage } from "next";
 import dynamic from "next/dynamic";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
-import Loading from "./loading";
-const HomeHeader = dynamic(() => import("@/components/2023/home/header"), {
-	loading: () => <Loading />,
-});
+const HomeHeader = dynamic(() => import("@/components/2023/home/header"));
 const Home: NextPage = () => {
 	const headersList = headers();
 	const referer = headersList.get("referer");
