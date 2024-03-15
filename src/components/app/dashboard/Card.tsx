06fd989cfc5af28ab7ae7acb802683ed7df32879
Alpha Romer Coma <alpha.coma.ict@gmail.com>
2024-03-15 23:17:30 +0800

    refactor: pre-commit hooks lint and git add

--- src/components/app/dashboard/Card.tsx
+++ src/components/app/dashboard/Card.tsx
@@ -1,52 +1,54 @@
 import {
-    Card,
-    CardBody,
-    CardFooter,
-    CardHeader,
-    Image,
-    Link,
+	Card,
+	CardBody,
+	CardFooter,
+	CardHeader,
+	Image,
+	Link,
 } from "@nextui-org/react";
 import { HiDotsHorizontal } from "react-icons/hi";
 
 const DasboardCard = () => {
-    return (
-        <Card className="w-full" radius="none">
-            <CardHeader className="flex justify-between">
-                <span className="flex gap-3">
-                    <Image
-                        alt="acm logo"
-                        height={40}
-                        radius="sm"
-                        src="/android-chrome-192x192.png"
-                        width={40}
-                    />
-                    <div className="flex flex-col">
-                        <p className="text-md text-panelColor font-bold">
-                            FEU Tech ACM
-                        </p>
-                        <p className="text-small font-bold">2024/01/12</p>
-                    </div>
-                </span>
-                <span className="text-3xl text-assets">
-                    <HiDotsHorizontal />
-                </span>
-            </CardHeader>
-            <CardBody>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat debitis consequuntur minus. Dolore debitis ab eius aperiam neque doloremque, eum saepe ipsum consectetur, animi voluptates, maxime repellendus praesentium? Deleniti, nihil!
-                </p>
-            </CardBody>
-            <CardFooter>
-                <Link
-                    isExternal
-                    showAnchorIcon
-                    className="text-default-900"
-                    href="https://github.com/nextui-org/nextui"
-                >
-                    Visit source code on GitHub.
-                </Link>
-            </CardFooter>
-        </Card>);
+	return (
+		<Card className="w-full" radius="none">
+			<CardHeader className="flex justify-between">
+				<span className="flex gap-3">
+					<Image
+						alt="acm logo"
+						height={40}
+						radius="sm"
+						src="/android-chrome-192x192.png"
+						width={40}
+					/>
+					<div className="flex flex-col">
+						<p className="text-md text-panelColor font-bold">FEU Tech ACM</p>
+						<p className="text-small font-bold">2024/01/12</p>
+					</div>
+				</span>
+				<span className="text-3xl text-assets">
+					<HiDotsHorizontal />
+				</span>
+			</CardHeader>
+			<CardBody>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+					debitis consequuntur minus. Dolore debitis ab eius aperiam neque
+					doloremque, eum saepe ipsum consectetur, animi voluptates, maxime
+					repellendus praesentium? Deleniti, nihil!
+				</p>
+			</CardBody>
+			<CardFooter>
+				<Link
+					isExternal
+					showAnchorIcon
+					className="text-default-900"
+					href="https://github.com/nextui-org/nextui"
+				>
+					Visit source code on GitHub.
+				</Link>
+			</CardFooter>
+		</Card>
+	);
 };
 
 export default DasboardCard;
--- src/components/wrapper/CenterComponent.tsx
+++ src/components/wrapper/CenterComponent.tsx
@@ -6,7 +6,9 @@ interface Props extends ChildrenProps {
 }
 const CenterComponent: React.FC<Props> = ({ children, isNotFixed }) => {
 	return (
-		<section className={`min-h-screen w-full flex justify-center items-center ${isNotFixed ? "" : "fixed"}`}>
+		<section
+			className={`min-h-screen w-full flex justify-center items-center ${isNotFixed ? "" : "fixed"}`}
+		>
 			<div className="flex justify-center w-full items-center h-full relative max-w-4xl">
 				<div className="sm:p-6 rounded-lg flex p-1 text-center w-full sm:w-9/12 gap-2 flex-col justify-center items-center sm:shadow-md sm:shadow-accents">
 					<Link href="/2023" className="h-12 w-64 relative">
