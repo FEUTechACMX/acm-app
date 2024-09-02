92435b8e462b2d9ef7e6218c498a359e2de3b309
Rab Karl Disu Colasino <150637860+ColasRab@users.noreply.github.com>
2024-09-02 15:41:09 +0800

    Update credits.ts
    Signed-off-by: Rab Karl Disu Colasino <150637860+ColasRab@users.noreply.github.com>

--- src/app/api/utils/undertaking-generator/credits.ts
+++ src/app/api/utils/undertaking-generator/credits.ts
@@ -3,13 +3,10 @@ Copyright FEU Tech ACM Student Chapter. All rights reserved.
 
 Contributors (ACM App Team):
 
-- Sir Abraham Magpantay   :  Adviser             :  https://www.facebook.com/aber.magpantay
-- Jericho Menguito        :  President           :  https://www.facebook.com/JMENGUITO28
-- Cyril Reigine Paruli    :  Vice President      :  https://www.linkedin.com/in/cyrilreigineparuli
-- Guennevere Rito         :  Project Manager     :  https://www.linkedin.com/in/guennevere-rito-990256255
-- Alpha Romer Coma        :  Software Developer  :  https://www.linkedin.com/in/alpharomercoma
-- Emily Tan Sanchez       :  Branding Officer    :  https://www.facebook.com/francischuegan
-- Francis Chuegan         :  Branding Officer    :  https://www.facebook.com/emilyats
+- Sir Jeneffer Sabonsolin  :  Adviser             :  https://www.linkedin.com/in/jeneffer-sabonsolin-70320329a
+- Csypress Ornos           :  President           :  https://www.linkedin.com/in/csypres-ornos-148528287
+- Alpha Romer Coma         :  Software Developer  :  https://www.linkedin.com/in/alpharomercoma
+- Rab Karl Colasino        :  Webmaster           :  https://www.linkedin.com/in/mr-colasino
 
 For further information, please contact us at <acm.feu.it@gmail.com>`;
 
